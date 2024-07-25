f7981b1c4991af23d3252502009740581c805eba
Nicholas Loren <lorennicosir@gmail.com>
2024-07-25 03:17:21 +0300

    Fixed bug

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'https://unpkg.com/three@0.112/examples/jsm/controls/OrbitControls.js'
 
 // Set up scene, camera, and renderer
 const scene = new THREE.Scene()
